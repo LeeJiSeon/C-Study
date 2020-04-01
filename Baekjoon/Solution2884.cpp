b4d538d6f82dc9dda17ff58d45edcf81dce634c1
LeeJiSun <j5119s@naver.com>
2020-04-01 12:27:40 +0900

    알람 시계

---
+++ Baekjoon/Solution2884.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int h, m;
+    cin >> h >> m;
+
+    if(m < 45) {
+        if(h == 0)  h = 23;
+        else    h--;
+        m += 15;
+    }
+    else    m -= 45;
+
+    cout << h << " " << m << endl;
+    return 0;
+}
